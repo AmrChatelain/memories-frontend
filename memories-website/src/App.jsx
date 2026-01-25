c7d4820585bbc29ec6ae7b261db7a474f7422dc3
Amr CHATELAIN <amr.chatelain@gmail.com>
2026-01-25 13:06:43 +0100

    routes

--- memories-website/src/App.jsx
+++ memories-website/src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Auth/Login"
+import SignUp from "./Pages/Auth/SingUp"
 
 function App() {
   return (
     <div>
-      app
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signUp" element={<SignUp />} />
+        </Routes>
+      </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
